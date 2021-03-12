b4b479c5d97be8f2a5e7cc47df9e0d2bea55190e
AnaLarios <alariosv01@iesalbarregas.es>
2021-03-12 12:37:45 +0100

    Líneas en método main

---
+++ ana_lariosDAW/src/ana_lariosDAW/Principal.java
@@ -0,0 +1,12 @@
+package ana_lariosDAW;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Este es un mensaje de la clase principal");	
+	System.out.println("Esta es la segunda línea de la clase principal");
+
+	}//fin main
+
+}//fin clase
